2d54ffb4c51df6c30c0f77d9f14a2a9d335cb487
ultimecreation <naserbechata@gmail.com>
2022-01-25 15:01:37 +0100

    duplicate teacher and save it as student project

--- src/Controller/ControllerProject.php
+++ src/Controller/ControllerProject.php
@@ -2,7 +2,9 @@
 
 namespace Interfaces\Controller;
 
+use User\Entity\User;
 use Interfaces\Entity\Project;
+use Interfaces\Entity\LtiProject;
 
 class ControllerProject extends Controller
 {
@@ -144,6 +146,103 @@ class ControllerProject extends Controller
                     return true;
                 }
                 return false;
+            },
+            'lti_teacher_duplicate_project'=>function() { 
+
+                // accept only POST request
+                if ($_SERVER['REQUEST_METHOD'] !== 'POST') return ["error" => "Method not Allowed"];
+
+                // accept only connected user
+                if (empty($_SESSION['id'])) return ["errorType" => "ltiDuplicateTeacherProjectNotRetrievedNotAuthenticated"];
+ 
+                // bind and sanitize incoming data
+                $userId = intval($_SESSION['id']);
+                $ltiCourseId = !empty($_POST['course_id']) ? $_POST['course_id'] : null;
+                $ltiResourceLinkId = !empty($_POST['resource_link_id']) ? $_POST['resource_link_id'] : null;
+                $projectLink = !empty($_POST['link']) ? htmlspecialchars(strip_tags(trim($_POST['link']))) : '';
+ 
+                // initialize empty $errors array
+                $errors = [];
+                if(empty($ltiCourseId)) $errors['ltiCourseIdInvalid'] = true;
+                if(empty($ltiResourceLinkId)) $errors['resourceLinkIdInvalid'] = true;
+                if(empty($projectLink)) $errors['projectLinkInvalid'] = true;
+ 
+                // some errors found, return them
+                if(!empty($errors)){
+                    return array('errors'=>$errors);
+                }
+               
+                // get the user
+                $user = $this->entityManager
+                                ->getRepository(User::class)->find($userId);
+                                
+                // get the project                        
+                $project = $this->entityManager
+                            ->getRepository('Interfaces\Entity\Project')
+                            ->findOneBy(array("link" => $projectLink));
+
+                // the project does not exists, return an error
+                if(!$project){
+                    return array('errorType'=> 'projectNotFoundWithProvidedLink');
+                }
+
+                // get the lti project from interfaces_lti_project table
+                $ltiProjectNotAlreadySubmitted = $this->entityManager
+                                    ->getRepository(LtiProject::class)
+                                    ->findOneBy(array(
+                                        'user' => $user->getId(),
+                                        'ltiCourseId' => $ltiCourseId,
+                                        'ltiResourceLinkId' => $ltiResourceLinkId,
+                                        'isSubmitted' => 0
+                                    ));
+
+                // no reference of this project in ltiProject ($user + $courseId + $ltiResourceId do not exists)
+                if(!$ltiProjectNotAlreadySubmitted){
+                    
+                    // we duplicate the project for this user and save it
+                    $projectDuplicated = new Project(
+                        $project->getName(), 
+                        $project->getDescription()
+                    );
+                    $projectDuplicated->setUser($user);
+                    $projectDuplicated->setDateUpdated();
+                    $projectDuplicated->setCode($project->getCode());
+                    $projectDuplicated->setCodeText($project->getCodeText());
+                    $projectDuplicated->setCodeManuallyModified($project->isCManuallyModified());
+                    $projectDuplicated->setPublic($project->isPublic());
+                    $projectDuplicated->setLink(uniqid());
+                    $projectDuplicated->setInterface($project->getInterface());
+                    $this->entityManager->persist($projectDuplicated);
+                    $this->entityManager->flush();
+
+                    // we create a ltiProject entry in interfaces_lti_projects and save it
+                    $ltiProject = new LtiProject();
+                    $ltiProject->setUser($user);
+                    $ltiProject->setUserProjectLink($projectDuplicated->getLink());
+                    $ltiProject->setLtiCourseId($ltiCourseId);
+                    $ltiProject->setLtiResourceLinkId($ltiResourceLinkId);
+                    $ltiProject->setIsSubmitted(false);
+
+                    $this->entityManager->persist($ltiProject);
+                    $this->entityManager->flush();
+
+                    // save data in session
+                    $_SESSION['lti_project_id'] = $ltiProject->getId();
+                    return  $projectDuplicated->jsonSerialize();
+                }
+
+                $userProject = $this->entityManager
+                            ->getRepository(Project::class)
+                            ->findOneByLink($ltiProjectNotAlreadySubmitted->getUserProjectLink());
+                
+                if(!$userProject){
+                    return array('errorType'=> "userProjectNotFound");
+                }
+
+                // save data in session
+                $_SESSION['lti_project_id'] = $ltiProjectNotAlreadySubmitted->getId();
+                return $userProject;
+                
             }
         );
     }
