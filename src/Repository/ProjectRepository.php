cbc13cba372dd4def3b7699bf45810bb2b98327d
nas-be <34138024+ultimecreation@users.noreply.github.com>
2023-06-19 16:26:22 +0200

    added authorFullname and authorId fields to dataset returned

--- src/Repository/ProjectRepository.php
+++ src/Repository/ProjectRepository.php
@@ -4,6 +4,7 @@ namespace Interfaces\Repository;
 
 use Doctrine\ORM\EntityRepository;
 use Interfaces\Entity\Project;
+use User\Entity\User;
 
 class ProjectRepository extends EntityRepository
 {
@@ -24,8 +25,9 @@ class ProjectRepository extends EntityRepository
     {
         $query = $this->getEntityManager()
             ->createQueryBuilder()
-            ->select("p.id, p.name, p.description, p.link, p.dateUpdated, p.interface, p.mode")
+            ->select("p.id, p.name, p.description, p.link, p.dateUpdated, p.interface, p.mode, CONCAT(u.firstname,' ',u.surname) AS authorFullname, u.id AS authorId")
             ->from(Project::class, 'p')
+            ->innerJoin(User::class,'u','WITH',"p.user=u.id")
             ->where('(p.public = :public AND p.deleted = :deleted AND p.interface = :interface)')
             ->setParameters(['public' => $data['public'], 'deleted' => $data['deleted'], 'interface' => $data['interface']])
             ->getQuery()
